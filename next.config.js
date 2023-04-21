0554d299f826add3c158eb3d5f26f0cc71944439
junkkie <jenkem029@gmail.com>
2023-04-21 11:24:52 +0900

    second

--- next.config.js
+++ next.config.js
@@ -4,3 +4,12 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+//next.config.js
+const debug = process.env.NODE_ENV !== 'production'
+const name = 'app_nextjs_typescript'
+
+module.exports = {
+  assetPrefix: !debug ? `/${name}/` : '',
+  basePath: '/app_nextjs_typesxript',
+}
